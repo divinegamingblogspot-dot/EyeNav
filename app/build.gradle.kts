b0d3bdf1b545931780c02fab904061c54f0e2bf9
divinegamingblogspot-dot <divinegamingblogspot@gmail.com>
2026-09-05 14:31:00 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.prince.eyenav"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.prince.eyenav"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+
+    implementation("androidx.camera:camera-core:1.5.0")
+    implementation("androidx.camera:camera-camera2:1.5.0")
+    implementation("androidx.camera:camera-lifecycle:1.5.0")
+    implementation("androidx.camera:camera-view:1.5.0")
+
+    implementation("com.google.mlkit:face-detection:16.1.7")
+}
