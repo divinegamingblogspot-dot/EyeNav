8bf37ad75da413ad8df2a9d37a9405f4427ca25b
divinegamingblogspot-dot <divinegamingblogspot@gmail.com>
2026-09-05 19:12:49 +0530

    EyeNavAccessibilityService.kt

--- app/src/main/java/com/prince/eyenav/EyeNavAccessibilityService.kt
+++ app/src/main/java/com/prince/eyenav/EyeNavAccessibilityService.kt
@@ -9,7 +9,6 @@ class EyeNavAccessibilityService : AccessibilityService() {
 
     companion object {
         var instance: EyeNavAccessibilityService? = null
-            private set
     }
 
     override fun onServiceConnected() {
@@ -17,84 +16,36 @@ class EyeNavAccessibilityService : AccessibilityService() {
         instance = this
     }
 
-    override fun onAccessibilityEvent(
-        event: AccessibilityEvent?
-    ) {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
     }
 
     override fun onInterrupt() {
     }
 
+    fun performEyeClick(x: Float, y: Float) {
+
+        val path = Path()
+        path.moveTo(x, y)
+
+        val gesture = GestureDescription.Builder()
+            .addStroke(
+                GestureDescription.StrokeDescription(
+                    path,
+                    0,
+                    50
+                )
+            )
+            .build()
+
+        dispatchGesture(
+            gesture,
+            null,
+            null
+        )
+    }
+
     override fun onDestroy() {
         instance = null
         super.onDestroy()
-    }
-
-    fun tap(x: Float, y: Float) {
-
-        val path = Path()
-
-        path.moveTo(x, y)
-
-        val stroke =
-            GestureDescription.StrokeDescription(
-                path,
-                0,
-                80
-            )
-
-        val gesture =
-            GestureDescription.Builder()
-                .addStroke(stroke)
-                .build()
-
-        dispatchGesture(
-            gesture,
-            null,
-            null
-        )
-    }
-
-    fun swipe(
-        startX: Float,
-        startY: Float,
-        endX: Float,
-        endY: Float
-    ) {
-
-        val path = Path()
-
-        path.moveTo(startX, startY)
-        path.lineTo(endX, endY)
-
-        val stroke =
-            GestureDescription.StrokeDescription(
-                path,
-                0,
-                500
-            )
-
-        val gesture =
-            GestureDescription.Builder()
-                .addStroke(stroke)
-                .build()
-
-        dispatchGesture(
-            gesture,
-            null,
-            null
-        )
-    }
-
-    fun goBack() {
-        performGlobalAction(
-            GLOBAL_ACTION_BACK
-        )
-    }
-
-    fun goHome() {
-        performGlobalAction(
-            GLOBAL_ACTION_HOME
-        )
     }
 }
