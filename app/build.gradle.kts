1e4257eeaccc981b5407218dc27011540febcd96
divinegamingblogspot-dot <divinegamingblogspot@gmail.com>
2026-09-13 15:14:54 +0530

    Bump Doc to Fusion 7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.prince.eyenav"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "6.0"
+        versionCode = 7
+        versionName = "7.0-DOC-FUSION"
     }
 }
 
